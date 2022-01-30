4f1b4b0fb8f56b264bf68597f5f84f5ee412a7d4
Emmanuel Ferte <eferte.omnilog@tf1.fr>
2022-01-30 21:56:12 +0100

    refacto

--- src/type/utils.ts
+++ src/type/utils.ts
@@ -1,4 +1,3 @@
-
 export type Dictionary<T = any> = { [key: string]: T };
 
 export type SetStateAction<S> = S | ((prevState: S) => S);
@@ -13,46 +12,14 @@ export function isNumber(value: any): value is number {
   return typeof value === "number";
 }
 export function isBoolean(value: any): value is boolean {
-  return (
-    typeof value === "boolean" || value instanceof Boolean || value === Boolean
-  );
+  return typeof value === "boolean" || value instanceof Boolean || value === Boolean;
 }
 export function isPrimitive(value: any): value is number | string | boolean {
   return isString(value) || isNumber(value) || isBoolean(value);
 }
-function isObject(value: any): value is object {
-  return value !== null && typeof value === "object";
-}
 export function isDictionary<T = any>(value: any): value is Dictionary<T> {
   return value !== null && typeof value === "object";
 }
-const objectToString = (o: any): string => Object.prototype.toString.call(o);
-
-export function isDate(value: any): value is Date {
-  return isObject(value) && objectToString(value) === "[object Date]";
-}
 export function isArray<T>(value: any): value is Array<T> {
   return Array.isArray(value);
-}
-export function isError(value: any): value is Error {
-  return (
-    isObject(value) &&
-    (objectToString(value) === "[object Error]" || value instanceof Error)
-  );
-}
-
-export function omit<T = any>(
-  value: T,
-  a: T[],
-  predicat?: (a: T, b: T) => boolean
-): T[] {
-  if (predicat) {
-    return a.filter((v) => {
-      return !predicat(v, value);
-    });
-  } else {
-    return a.filter((v) => {
-      return v !== value;
-    });
-  }
 }
